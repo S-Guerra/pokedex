5f6d3c9d535a5240456e641b413ea7115219b0a6
S. <stelio.guerra@gmail.com>
2025-05-15 14:10:48 +0200

    Add basic component structure

---
+++ pokedex-app/src/components/LeftPanel/ControlsPanel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ControlsPanel() {
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ pokedex-app/src/components/RightPanel/TypeScreens.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TypeScreens() {
+    return (
+        <div>
+
+        </div>
+    )
+}
