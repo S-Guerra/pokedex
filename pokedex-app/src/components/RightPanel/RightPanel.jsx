90b0a9e8c6fba9536f3279ed8936dcc4d9d9329b
S. Guerra <stelio.guerra@gmail.com>
2025-05-21 15:44:11 +0200

    Fix margin animation gap

--- pokedex-app/src/components/RightPanel/RightPanel.jsx
+++ pokedex-app/src/components/RightPanel/RightPanel.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../styles/layout/right-panel.scss";
 
 export default function RightPanel({ isOpen, setIsOpen }) {
-    const toggleOpen = () => setIsOpen(!isOpen);
+    const [bookState, setBookState] = useState("")
+
+    const toggleOpen = () => {
+        const newIsOpen = !isOpen;
+        setIsOpen(newIsOpen);
+        setBookState(newIsOpen ? "open" : "closed");
+    };
 
     return (
         <div className="panel-right-wrapper">
             <div className="placeholder"></div>
-            <div className={`book-cover ${isOpen ? "open" : ""}`} onClick={toggleOpen}>
+            <div className={`book-cover ${bookState}`} onClick={toggleOpen}>
                 <div className="top">
                     <div className="red"></div>
                     <div className="sideways"></div>
