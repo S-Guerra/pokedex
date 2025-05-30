4b0e95924f4a94bfda666199ecebf90e922944f2
S. Guerra <stelio.guerra@gmail.com>
2025-05-30 16:07:51 +0200

    Update add layout for backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const { Pool } = require("pg");
+const app = express();
+const port = 3001;
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+});
+
+app.get("/api/pokemon", async (req, res) => {
+    try {
+        const { rows } = await pool.query("SELECT * FROM pokemon_gen1");
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Database query failed" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
