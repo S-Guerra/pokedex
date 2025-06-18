ab03cf1590eebf178fe180e922ea1bd82fef5fbb
S. Guerra <stelio.guerra@gmail.com>
2025-06-18 19:21:19 +0200

    Add GitHub link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
   return (
     <>
       <Pokedex />
+      <a className="github" href="https://github.com/S-Guerra/pokedex">
+        <img src="https://img.shields.io/badge/GitHub-181717?logo=github&logoColor=white&style=flat" alt="GitHub Badge"></img>
+      </a>
       <p className="credits">S. Guerra - 2025</p>
     </>
   )
